1e2944128f6927811b6264968a8ef56feb333c84
Mridul Rastogi <mridul.rastogi1@ibm.com>
2025-11-10 11:56:27 +0530

    Changes

---
+++ Learn React/react-application/src/Message.tsx
@@ -0,0 +1,8 @@
+function Message() {
+  const name = "Mridul";
+  // This is JSX JavaScript XMLSyntax
+  if (name) return <h1>Hello {name}</h1>;
+  return <h1>Hello World</h1>;
+}
+
+export default Message;
